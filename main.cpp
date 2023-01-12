279560f7362a4a1add2c717b4cda3388d0779144
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-12 00:45:18 -0500

    created a window

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <glad/glad.h> // helps loads drivers
+#include <GLFW/glfw3.h> // window system
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); // prototype for window resize func
+
+int main () {
+    // Initialization and configuration of GLFW
+    glfwInit(); // initialize the GLFW library
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Telling GLFW we want to use OpenGL version 3 ...
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // point 3 so version 3,3
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // telling GLFW we want to use OpenGL Core profile
+    // Making a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL); // generate a window
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); // set it as the context for the current thread
+
+    // Loading GLAD
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+
+    // OpenGL window configuration
+    glViewport(0, 0, 800, 600); // tell OpenGL the size of the viewport
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // setting the callback func for window resize
+    // Render loop
+    while (!glfwWindowShouldClose(window)) { // checks if GLFW has been told to close the window
+        glfwSwapBuffers(window); // swap the color buffer (represents pixels on the window)
+        glfwPollEvents(); // checks if any io events have been triggered
+    }
+
+    glfwTerminate(); // clean up GLFW resources
+    return 0;
+}
+
+// window resize callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
