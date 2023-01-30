81c88868354b617e4bdc71a713438ae6c1ef4154
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-29 22:37:41 -0500

    rotate the cube on a more complex axis

--- main.cpp
+++ main.cpp
@@ -132,8 +132,7 @@ int main () {
 
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(sin(timeValue - startTimeValue) - 0.25f, 0.0f, 0.0f));
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        model = glm::rotate(model, timeValue, glm::vec3(0.0, 0.0, -1.0));
+        model = glm::rotate(model, timeValue, glm::vec3(1.0, 1.0, -1.0));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
 
         glm::mat4 view = glm::mat4(1.0f);
